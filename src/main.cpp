9433b3a32182d32f17e49e8f0a400841e71cd46e
Caleb <calebkugel1@gmail.com>
2022-11-27 23:14:11 -0600

    started opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,16 @@
 #include <opencv2/core.hh>
 #include <iostream>
 
+using namespace std;
+
 int main() {
-    std::cout << "hello" << std::endl;
+    string im1path;
+    string im2path;
+
+    Mat im1 = cv::imread(im1path);
+    Mat im2 = cv::imread(im2path);
+    
+    
+    
     return -1;
 }
